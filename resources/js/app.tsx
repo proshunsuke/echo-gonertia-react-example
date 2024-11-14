06ae9aeb5966f6372878b5780991fd99bf33dd32
proshunsuke <shunsuke0901@gmail.com>
2024-11-14 22:31:42 +0900

    use persistent layouts

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,22 +1,28 @@
 import { Layout } from "@/Components/Layout/index";
+import type { Page } from "@inertiajs/core";
 import { createInertiaApp } from "@inertiajs/react";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import type { ReactElement } from "react";
 import { createRoot } from "react-dom/client";
 import "../css/app.css";
 
+type PageType = Page & {
+	default: {
+		layout?: (page: ReactElement) => ReactElement;
+	};
+};
+
 createInertiaApp({
-	resolve: (name) =>
-		resolvePageComponent(
-			`./Pages/${name}.tsx`,
-			import.meta.glob("./Pages/**/*.tsx"),
-		),
+	resolve: async (name) => {
+		const pages = import.meta.glob<Promise<PageType>>("./Pages/**/*.tsx");
+		const pageImport = pages[`./Pages/${name}.tsx`];
+		const page = await pageImport();
+		page.default.layout =
+			page.default.layout || ((page: ReactElement) => <Layout>{page}</Layout>);
+		return page;
+	},
 	setup({ el, App, props }) {
 		const root = createRoot(el);
 
-		root.render(
-			<Layout>
-				<App {...props} />
-			</Layout>,
-		);
+		root.render(<App {...props} />);
 	},
 });
